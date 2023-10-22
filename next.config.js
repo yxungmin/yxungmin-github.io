2764f8b3dcb8f0af112f97dca55f490d6f2df15d
youngmin <g369369g@gmail.com>
2023-10-22 16:16:24 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    trailingSlash: true,
+    output: 'export',
+    distDir: 'build',
+}
 
 module.exports = nextConfig
